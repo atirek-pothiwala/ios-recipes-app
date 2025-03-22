8f86db47adbc1d536e1c64f5493c51df306824da
Atirek Pothiwala <atirekpothiwala@gmail.com>
2025-03-22 22:57:38 +0530

    Initial Commit

---
+++ recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  recipes
+//
+//  Created by Atirek Pothiwala on 22/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ recipes/recipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipesApp.swift
+//  recipes
+//
+//  Created by Atirek Pothiwala on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct recipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
